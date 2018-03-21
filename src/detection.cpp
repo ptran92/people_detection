cacd9f72d7e2ec18bebb98321e0b09c3db5daa12
Phu Tran <tran.thanh.bao.phu92@gmail.com>
2018-03-21 03:52:53 +0000

    Add code to read and write video frame with gstreamer

--- src/detection.cpp
+++ src/detection.cpp
@@ -5,35 +5,26 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 
-#define OUTPUT_FPS    20
-
 int main(int argc, char* argv[]){
-
-  std::cout << "Hello world" << "\n";
-
-  cv::VideoCapture capture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)I420, framerate=(fraction)120/1 ! \
-			nvvidconv flip-method=6 ! video/x-raw, format=(string)BGRx ! \
-			videoconvert ! video/x-raw, format=(string)BGR ! \
-			appsink");
+	
+	/* Read video stream */
+  cv::VideoCapture capture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)I420, framerate=(fraction)60 ! nvvidconv ! video/x-raw, format=(string)I420 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
 
   assert(capture.isOpened() == true);
 
-  cv::VideoWriter outputVid("appsrc ! autovideoconvert ! omxh265enc ! matroskamux ! filesink location=camera.mkv",
-                              // CV_FOURCC('A','V','C','1'),
-                              0,
-                              OUTPUT_FPS,
-                              cv::Size((int)capture.get(cv::CAP_PROP_FRAME_WIDTH),(int)capture.get(cv::CAP_PROP_FRAME_HEIGHT)),
-                              true
-                            );
+	/* Write video to file */
+  cv::VideoWriter outputVid("appsrc ! videoconvert ! omxh264enc ! mpegtsmux ! filesink location=camera.mp4 ", 0, 60, cv::Size((int)capture.get(cv::CAP_PROP_FRAME_WIDTH),(int)capture.get(cv::CAP_PROP_FRAME_HEIGHT)),true);
+
   assert(outputVid.isOpened() == true);
 
+	/* Start reading */
   std::cout << "Start recording" << '\n';
 
   cv::Mat inFrame;
-  while(capture.read(inFrame) && outputVid.get(cv::CAP_PROP_FRAME_COUNT) <= 10 ){
-    std::cout << "get " << outputVid.get(cv::CAP_PROP_FRAME_COUNT) << '\n';
+	int count = 0;
+  while(capture.read(inFrame) && count <= 100 ){
     outputVid.write(inFrame);
-    cv::waitKey(1000);
+		count++;
   }
 
   std::cout << "Stop recording" << '\n';
